adff8bccc4c0fbb830b61557af6871c1e677daaf
taichihub <taichikuniv21@gmail.com>
2024-11-27 18:14:12 +0900

    useViewをApp.jsxに統一

--- src/hooks/useView.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import constants from "../constants/constants";
-
-export const useView = () => {
-  const [view, setView] = useState(constants.viewType.list);
-  const [currentMemo, setCurrentMemo] = useState(null);
-
-  return { view, setView, currentMemo, setCurrentMemo };
-};
