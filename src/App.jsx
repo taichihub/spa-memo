aa7e48e5a138ea6234c146fd58ca75d698fec606
taichihub <taichikuniv21@gmail.com>
2024-12-01 21:16:08 +0900

    ログイン・非ログイン状態ごとに機能を制限

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,76 @@
-import React from "react";
-import { useState } from "react";
-import { useMemo } from "./hooks/useMemo";
+import React, { useState, useContext } from "react";
+import { useMemo as useMemoHook } from "./hooks/useMemo";
 import MemoList from "./components/MemoList";
 import MemoEditor from "./components/MemoEditor";
 import Header from "./components/Header";
 import labels from "./locales/labels";
+import messages from "./locales/messages";
 import constants from "./constants/constants";
+import SessionProvider, { sessionContext } from "./components/SessionProvider";
 import "./App.css";
 
-const App = () => {
+const AppSetUp = () => {
   const [view, setView] = useState(constants.viewType.list);
   const [currentMemo, setCurrentMemo] = useState(null);
-  const { memos, addMemo, saveMemo, deleteMemo } = useMemo();
+  const { memos, addMemo, saveMemo, deleteMemo } = useMemoHook();
+  const { session } = useContext(sessionContext);
+
+  const handleNewMemo = () => {
+    if (session === "logout") {
+      alert(messages.window.LogInToAddMemo);
+      return;
+    }
+    addMemo(setCurrentMemo);
+    setView(constants.viewType.create);
+  };
+
+  const renderMemoList = () => (
+    <section>
+      <button onClick={handleNewMemo}>{labels.newMemoButton}</button>
+      <MemoList
+        memos={memos}
+        onMemoClick={(memo) => {
+          setCurrentMemo(memo);
+          setView(constants.viewType.edit);
+        }}
+      />
+    </section>
+  );
+
+  const renderMemoEditor = () => (
+    <section>
+      <MemoEditor
+        memo={currentMemo}
+        onSave={(memo) => {
+          saveMemo(memo);
+          setView(constants.viewType.list);
+        }}
+        onDelete={() => {
+          deleteMemo(currentMemo.id);
+          setView(constants.viewType.list);
+        }}
+        onCancel={() => setView(constants.viewType.list)}
+      />
+    </section>
+  );
 
   return (
     <div>
       <Header />
       <main>
-        {view === constants.viewType.list && (
-          <section>
-            <button
-              onClick={() => {
-                addMemo(setCurrentMemo);
-                setView(constants.viewType.create);
-              }}
-            >
-              {labels.newMemoButton}
-            </button>
-            <MemoList
-              memos={memos}
-              onMemoClick={(memo) => {
-                setCurrentMemo(memo);
-                setView(constants.viewType.edit);
-              }}
-            />
-          </section>
-        )}
-        {(view === constants.viewType.create ||
-          view === constants.viewType.edit) &&
-          currentMemo && (
-            <section>
-              <MemoEditor
-                memo={currentMemo}
-                onSave={(memo) => {
-                  saveMemo(memo);
-                  setView(constants.viewType.list);
-                }}
-                onDelete={() => {
-                  deleteMemo(currentMemo.id);
-                  setView(constants.viewType.list)
-                }}
-                onCancel={() => setView(constants.viewType.list)}
-              />
-            </section>
-          )}
+        {view === constants.viewType.list && renderMemoList()}
+        {(view === constants.viewType.create || view === constants.viewType.edit) &&
+          currentMemo &&
+          renderMemoEditor()}
       </main>
     </div>
   );
 };
+
+const App = () => (
+  <SessionProvider>
+    <AppSetUp />
+  </SessionProvider>
+);
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import texts from "../locales/texts";
+import labels from "../locales/labels";
+import { sessionContext } from "./SessionProvider";
 
-const Header = () => (
-  <header>
-    <h1>{texts.appTitle}</h1>
-  </header>
-);
+const Header = () => {
+  const { session, login, logout } = useContext(sessionContext);
+  
+  return (
+    <header>
+      <h1>{texts.appTitle}</h1>
+      <button onClick={session === "login" ? logout : login}>
+        {session === "login" ? labels.logOutButton : labels.logInButton}
+      </button>
+    </header>
+  );
+};
 
 export default Header;
