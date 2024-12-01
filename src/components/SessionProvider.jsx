4f0c6fd17723b45802c392e08be51f3e27914c7d
taichihub <taichikuniv21@gmail.com>
2024-12-01 21:15:44 +0900

    ログイン状態をもつプロバイダを定義

---
+++ src/components/SessionProvider.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { createContext } from "react";
+
+export const sessionContext = createContext();
+
+const SessionProvider = ({ children }) => {
+  const [session, setSession] = useState('logout');
+  
+  const login = () => setSession('login');
+  const logout = () => setSession('logout');
+
+  return (
+    <sessionContext.Provider value={{ session, login, logout }}>
+      {children}
+    </sessionContext.Provider>
+  );
+};
+
+export default SessionProvider;
