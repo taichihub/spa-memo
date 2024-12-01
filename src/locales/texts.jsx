85fd3e431def6c512c6ca622b7a0cd2a08256ab7
taichihub <taichikuniv21@gmail.com>
2024-12-01 21:15:06 +0900

    メッセージ等を定義

--- src/locales/texts.jsx
+++ src/locales/texts.jsx
@@ -3,6 +3,7 @@ const texts = {
   listMemosTitle: "メモ一覧",
   newMemoTitle: "メモ作成",
   editMemoTitle: "メモ編集",
+  showMemoTitle: "メモ閲覧",
   editorPlaceholder: "メモ内容を入力してください...",
   noContent: "無題",
   emptyString: "",
